2f92c7f92d783c94cf6227f323c1ada9f29e51d1
tiredmagician <ethancheong@hotmail.sg>
2020-12-25 17:13:22 +0800

    Updating Person class

--- traditionalMarriage/traditionalMarriage.py
+++ traditionalMarriage/traditionalMarriage.py
@@ -1,5 +1,6 @@
 import random
 import collections # For improved time complexity
+import copy
 
 def createPreferences(identity, n):
     pref_list = [i for i in range(n)]
@@ -19,11 +20,15 @@ class Person:
     def getPreferences(self):
         return self.preferences
 
+    def setPreferences(self, input):
+        # Takes a list as input, allowing us to set custom preferences
+        self.preferences = collections.deque(input)
+
 class Man(Person):
     def __init__(self, identity, n):
         # Representation of a man
         super().__init__(identity, n)
-        self.remaining_preferences = self.preferences
+        self.remaining_preferences = copy.copy(self.preferences)
 
     def getRemainingPreferences(self):
         return self.remaining_preferences
@@ -43,6 +48,10 @@ class Woman(Person):
     def addToList(self, man):
         self.man_list.append(man)
 
+    def clearManList(self):
+        # We need to reset the list of men visiting after every iteration
+        self.man_list = []
+
     def chooseMan(self):
         # Given a list of men, pick the one whos identity is the leftmost entry
         # from your list of preferences.
@@ -56,19 +65,40 @@ class Woman(Person):
         rejected_men.remove(chosen_man)
         return rejected_men
 
-    def clearManList(self):
-        # We need to reset the list of men visiting after every iteration
-        self.man_list = []
-
 def initializePeople(n):
     # Initialize arrays of men and women
     men = [Man(i, n) for i in range(n)]
     women = [Woman(i, n) for i in range(n)]
     return (men,women)
 
+def nightIteration(men_array, women_array):
+    # Function for each night that passes
+    for woman in women_array:
+        woman.clearManList()
+
+    for man in men_array:
+        print("Man " + str(man.getIdentity()) + " has the preferences "
+        + ','.join([str(i) for i in man.getRemainingPreferences()]) + ".")
+        favourite_woman = man.getRemainingPreferences()[0]
+        print("Man " + str(man.getIdentity()) + " is visiting woman "
+        + str(favourite_woman) + "!")
+        women_array[favourite_woman].addToList(man.getIdentity()) # add the men visiting them to their man_list
+
+    for woman in women_array:
+        if woman.getManList():
+            print("Woman " + str(woman.getIdentity())
+            + " is being visited by man/men "
+            + ','.join([str(i) for i in woman.getManList()]) + "!")
+        preferred_man = woman.chooseMan()
+        if preferred_man is not None:
+            print("Woman " + str(woman.getIdentity()) + " chose man "
+            + str(preferred_man) + "!")
+            rejected_men = woman.getRejectedMen(preferred_man)
+            for rejected_man in rejected_men:
+                men_array[rejected_man].getRejected()
+
 def traditionalMarriage(men, women):
-    # Implementation of the algorithm. Takes in arrays of men and women, and
-    # returns the arrays with married pairs.
+    # Implementation of the algorithm
     if len(men) != len(women):
         print("We need the same number of men and women!")
     nights = 0
@@ -82,32 +112,5 @@ def traditionalMarriage(men, women):
             nights += 1
             print(str(nights) + " nights have passed!")
 
-def nightIteration(men_array, women_array):
-    # Function for each night that passes
-    # Note: This is VERY inefficient due to the number of nested for loops -
-    # Although I've decided to keep them in because they will help with
-    # visualization later - a good representation for how we might compute the
-    # algorithm by hand. I tried this with 10000 people and it crashed my
-    #laptop. If you'd like to optimize this, change it later - we can probably
-    # improve it using numpy and matrix algebra!
-    for woman in women_array:
-        woman.clearManList()
-
-    for man in men_array:
-        print("Man " + str(man.getIdentity()) + " has the preferences " + ','.join([str(i) for i in man.getRemainingPreferences()]) + ".")
-        favourite_woman = man.getRemainingPreferences()[0]
-        print("Man " + str(man.getIdentity()) + " is visiting woman " + str(favourite_woman) + "!")
-        women_array[favourite_woman].addToList(man.getIdentity()) # add the men visiting them to their man_list
-
-    for woman in women_array:
-        if woman.getManList():
-            print("Woman " + str(woman.getIdentity()) + " is being visited by man/men " + ','.join([str(i) for i in woman.getManList()]) + "!")
-        preferred_man = woman.chooseMan()
-        if preferred_man is not None:
-            print("Woman " + str(woman.getIdentity()) + " chose man " + str(preferred_man) + "!")
-            rejected_men = woman.getRejectedMen(preferred_man)
-            for rejected_man in rejected_men:
-                men_array[rejected_man].getRejected()
-
 # The * here expands the tuple made by initializePairs() and sets them as arguments
-traditionalMarriage(*initializePeople(100))
+traditionalMarriage(*initializePeople(5))
