bebc8697d91011f71e6e6b582ab02716cff2252f
ethan-cheong <ethancheong@hotmail.sg>
2020-12-25 15:13:50 +0800

    Updating traditional marriage algorithm

--- traditionalMarriage/traditionalMarriage.py
+++ traditionalMarriage/traditionalMarriage.py
@@ -38,7 +38,7 @@ class Woman(Person):
         self.man_list = []
 
     def getManList(self):
-        print(self.man_list)
+        return self.man_list
 
     def addToList(self, man):
         self.man_list.append(man)
@@ -61,20 +61,33 @@ def traditionalMarriage(n):
     # Initialize arrays of men and women
     men = [Man(i, n) for i in range(n)]
     women = [Woman(i, n) for i in range(n)]
-
     nights = 0 # counter for number of nights
-
-    for man in men:
+    
+    # Iterate until each woman has exactly one man visiting them.
+    while all(len(woman.getManList()) != 1 for woman in women):
+        for woman in women:
+            woman.clearManList()
+        visitIteration(men, women)
+    
+def visitIteration(men_array, women_array):
+    for man in men_array:
+        print("Man " + str(man.getIdentity()) + " has the preferences " + ','.join([str(i) for i in man.getRemainingPreferences()]) + ".")
         favourite_woman = man.getRemainingPreferences()[0]
-        women[favourite_woman].addToList(man.getIdentity()) # add the men visiting them to their man_list
+        print("Man " + str(man.getIdentity()) + " is visiting woman " + str(favourite_woman) + "!")
+        women_array[favourite_woman].addToList(man.getIdentity()) # add the men visiting them to their man_list
 
-    for woman in women:
+    for woman in women_array:
+        if woman.getManList():
+            print("Woman " + str(woman.getIdentity()) + " is being visited by man/men " + ','.join([str(i) for i in woman.getManList()]) + "!")
         preferred_man = woman.chooseMan()
-        rejected_men = woman.getRejectedMen(preferred_man)
-        for rejected_man in rejected_men:
-            men[rejected_man].getRejected()
+        if preferred_man is not None:
+            print("Woman " + str(woman.getIdentity()) + " chose man " + str(preferred_man) + "!")
+            rejected_men = woman.getRejectedMen(preferred_man)
+            for rejected_man in rejected_men:
+                men_array[rejected_man].getRejected()
+        
 
-    for man in men:
-        print(self.remaining_preferences())
+# Note: This is VERY inefficient due to the number of nested for loops - Although I've decided to keep them in because they will help with visualization later - a good representation for how we might compute the algorithm by hand. I tried this with 10000 people and it crashed my laptop. If you'd like to optimize this, change it later!
+
 
 traditionalMarriage(5)
